9f7ad80ae8c34779a4e72a630d986cc2dd841286
Pascale Beier <mail@pascalebeier.de>
2017-06-25 23:25:19 +0200

    Magnific Popup setup

--- Resources/Public/Es6/vendor.js
+++ Resources/Public/Es6/vendor.js
@@ -2,3 +2,26 @@ window.$ = window.jQuery = require('jquery');
 
 require('magnific-popup/dist/jquery.magnific-popup');
 require('bootstrap-sass/assets/javascripts/bootstrap');
+
+$(function() {
+    $('a[rel*="lightbox"]').magnificPopup({
+        type: 'image',
+        tLoading: 'Lade Bild...',
+        tClose: 'Schließen (Esc)',
+        image: {
+            titleSrc: function(item) {
+                var title = item.el.attr('title');
+                var description = item.el.attr('alt');
+                return ((title)?title:'') + ((description)?'<small>'+ description +'</small>':'');
+            }
+        },
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1],
+            tCounter: '%curr% von %total%',
+            tPrev: 'Zurück (Linke Pfeiltaste)',
+            tNext: 'Vorwärts (Rechte Pfeiltaste)'
+        }
+    });
+});
